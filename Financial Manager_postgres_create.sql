5b407a497f8968916385a633e7e8239863e200bc
Kristinadiug <54942199+Kristinadiug@users.noreply.github.com>
2020-11-04 08:58:18 +0200

    Edit database

--- Financial Manager_postgres_create.sql
+++ Financial Manager_postgres_create.sql
@@ -2,19 +2,14 @@ CREATE TABLE "User" (
 	"Id" serial NOT NULL,
 	"Name" varchar(255) NOT NULL,
 	"Surname" varchar(255) NOT NULL,
-	"Occupation" integer NOT NULL,
+	"Occupation" VARCHAR(255),
 	"Photo" BINARY,
+	"Default Currency" integer NOT NULL,
+	"Default Language" integer NOT NULL,
+	"Username" VARCHAR(255) NOT NULL UNIQUE,
+	"Password" VARCHAR(255) NOT NULL,
+	"Salt" VARCHAR(255) NOT NULL,
 	CONSTRAINT "User_pk" PRIMARY KEY ("Id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "Occupation" (
-	"Id" serial NOT NULL,
-	"Name" varchar(255) NOT NULL UNIQUE,
-	CONSTRAINT "Occupation_pk" PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
 );
@@ -44,13 +39,18 @@ CREATE TABLE "Currency Converter" (
 
 
 
-CREATE TABLE "Single Transaction" (
+CREATE TABLE "Transaction" (
 	"Id" serial NOT NULL,
 	"Categoty" integer NOT NULL,
 	"Amount" DECIMAL NOT NULL,
 	"Currency" integer NOT NULL,
 	"User" integer NOT NULL,
-	CONSTRAINT "Single Transaction_pk" PRIMARY KEY ("Id")
+	"Is_spending" BOOLEAN NOT NULL,
+	"Period" integer,
+	"Remark" VARCHAR(255),
+	"Date" DATE NOT NULL,
+	"Is_planned" BOOLEAN NOT NULL,
+	CONSTRAINT "Transaction_pk" PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
 );
@@ -81,35 +81,28 @@ CREATE TABLE "Payment Period" (
 
 
 
-CREATE TABLE "Regular Transaction" (
-	"Id" serial NOT NULL,
-	"Categoty" integer NOT NULL,
-	"Amount" DECIMAL NOT NULL,
-	"Currency" integer NOT NULL,
-	"User" integer NOT NULL,
-	"Period" integer NOT NULL,
-	CONSTRAINT "Regular Transaction_pk" PRIMARY KEY ("Id")
+CREATE TABLE "Language" (
+	"Id" integer NOT NULL,
+	"Name" VARCHAR(255) NOT NULL,
+	CONSTRAINT "Language_pk" PRIMARY KEY ("Id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-ALTER TABLE "User" ADD CONSTRAINT "User_fk0" FOREIGN KEY ("Occupation") REFERENCES "Occupation"("Id");
-
+ALTER TABLE "User" ADD CONSTRAINT "User_fk0" FOREIGN KEY ("Default Currency") REFERENCES "Currency"("Code");
+ALTER TABLE "User" ADD CONSTRAINT "User_fk1" FOREIGN KEY ("Default Language") REFERENCES "Language"("Id");
 
 
 ALTER TABLE "Currency Converter" ADD CONSTRAINT "Currency Converter_fk0" FOREIGN KEY ("From") REFERENCES "Currency"("Code");
 ALTER TABLE "Currency Converter" ADD CONSTRAINT "Currency Converter_fk1" FOREIGN KEY ("To") REFERENCES "Currency"("Code");
 
-ALTER TABLE "Single Transaction" ADD CONSTRAINT "Single Transaction_fk0" FOREIGN KEY ("Categoty") REFERENCES "Category"("id");
-ALTER TABLE "Single Transaction" ADD CONSTRAINT "Single Transaction_fk1" FOREIGN KEY ("Currency") REFERENCES "Currency"("Code");
-ALTER TABLE "Single Transaction" ADD CONSTRAINT "Single Transaction_fk2" FOREIGN KEY ("User") REFERENCES "User"("Id");
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_fk0" FOREIGN KEY ("Categoty") REFERENCES "Category"("id");
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_fk1" FOREIGN KEY ("Currency") REFERENCES "Currency"("Code");
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_fk2" FOREIGN KEY ("User") REFERENCES "User"("Id");
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_fk3" FOREIGN KEY ("Period") REFERENCES "Payment Period"("Id");
 
 
 
-ALTER TABLE "Regular Transaction" ADD CONSTRAINT "Regular Transaction_fk0" FOREIGN KEY ("Categoty") REFERENCES "Category"("id");
-ALTER TABLE "Regular Transaction" ADD CONSTRAINT "Regular Transaction_fk1" FOREIGN KEY ("Currency") REFERENCES "Currency"("Code");
-ALTER TABLE "Regular Transaction" ADD CONSTRAINT "Regular Transaction_fk2" FOREIGN KEY ("User") REFERENCES "User"("Id");
-ALTER TABLE "Regular Transaction" ADD CONSTRAINT "Regular Transaction_fk3" FOREIGN KEY ("Period") REFERENCES "Payment Period"("Id");
 
