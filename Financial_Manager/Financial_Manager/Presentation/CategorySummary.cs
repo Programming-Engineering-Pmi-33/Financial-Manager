f68baa54548a0e2a9e30b94acbf7471426ec482c
DumDumich <63633888+DumDumich@users.noreply.github.com>
2020-12-18 00:09:13 +0200

    graphics

---
+++ Financial_Manager/Financial_Manager/Presentation/CategorySummary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Manager
+{
+    public class CategorySummary : System.Collections.ObjectModel.Collection<PiePoint>
+    {
+        public CategorySummary()
+        {
+            Add(new PiePoint
+            {
+                Name = "Food",
+                Share = 26
+            });
+            Add(new PiePoint
+            {
+                Name = "Clothes",
+                Share = 36
+            });
+            Add(new PiePoint
+            {
+                Name = "Gifts",
+                Share = 24
+            });
+            Add(new PiePoint
+            {
+                Name = "Transport",
+                Share = 25
+            });
+            Add(new PiePoint
+            {
+                Name = "Books",
+                Share = 27
+            });
+
+        }
+    }
+}
---
+++ Financial_Manager/Financial_Manager/Presentation/PiePoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Manager
+{
+    public class PiePoint
+    {
+        public string Name { get; set; }
+        public int Share { get; set; }
+    }
+}
